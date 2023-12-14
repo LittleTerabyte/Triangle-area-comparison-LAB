e69bcedc9b657cec36c6a2e5a7bc1b97403a8d3b
Cam <52989181+LittleTerabyte@users.noreply.github.com>
2023-12-14 15:27:15 -0600

    Base code

---
+++ Triangle_area_comparison_LAB.py
@@ -0,0 +1,33 @@
+class Triangle:   
+    def __init__(self):
+        self.base = 0
+        self.height = 0
+
+    def set_base(self, user_base):
+        self.base = user_base
+
+    def set_height(self, user_height):
+        self.height = user_height
+   
+    def get_area(self):
+        area = 0.5 * self.base * self.height
+        return area
+   
+    def print_info(self):
+        print('Base: {:.2f}'.format(self.base))
+        print('Height: {:.2f}'.format(self.height))
+        print('Area: {:.2f}'.format(self.get_area()))
+
+if __name__ == "__main__":
+    triangle1 = Triangle()
+    triangle2 = Triangle()
+
+    # TODO: Read and set base and height for triangle1 (use set_base() and set_height())
+      
+    # TODO: Read and set base and height for triangle2 (use set_base() and set_height())
+      
+    # TODO: Determine larger triangle (use get_area())
+      
+    print('Triangle with larger area:')
+    # TODO: Output larger triangle's info (use print_info())
+
